9636d86cd9992112f41c70dde0c5127ed8d47442
Nauzet Aduen Hernández Hernández <nauzet.aduen@gmail.com>
2018-08-12 14:50:52 +0100

    ADD DateConverter class to allow mapping

--- app/src/main/java/com/aduen/nauzet/debtcontrol/database/AppDatabase.java
+++ app/src/main/java/com/aduen/nauzet/debtcontrol/database/AppDatabase.java
@@ -3,9 +3,11 @@ package com.aduen.nauzet.debtcontrol.database;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
 import android.content.Context;
 
 @Database(entities = {DebtEntry.class}, version = 1, exportSchema = false)
+@TypeConverters(DateConverter.class)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static final Object LOCK = new Object();
