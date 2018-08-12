ec8c06a5ad33f8fd11cc7f2ca1970d235aa667d3
Nauzet Aduen Hernández Hernández <nauzet.aduen@gmail.com>
2018-08-12 15:16:05 +0100

    ADD AddDebtActivity

--- app/src/main/java/com/aduen/nauzet/debtcontrol/MainActivity.java
+++ app/src/main/java/com/aduen/nauzet/debtcontrol/MainActivity.java
@@ -1,13 +1,29 @@
 package com.aduen.nauzet.debtcontrol;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton mFloatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mFloatingActionButton = findViewById(R.id.fab);
+
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AddDebtActivity.class));
+            }
+        });
     }
 }
